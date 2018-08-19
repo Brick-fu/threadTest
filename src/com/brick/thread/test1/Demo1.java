9cf759844f1cd5f14e390aee2d67ef85b8d81a8b
Brick-fu <liuxingtianze@163.com>
2018-08-19 22:50:39 +0800

    8.19.1

--- src/com/brick/thread/test1/Demo1.java
+++ src/com/brick/thread/test1/Demo1.java
@@ -10,7 +10,7 @@ public class Demo1 implements Runnable{
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("线程启动了！！！");
+            System.out.println("线程启动了！");
 
         }
     }
